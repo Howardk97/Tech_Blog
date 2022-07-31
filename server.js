24d7805c8a579a3428c5b947c239acf133ddb40b
KimboSlice <kimberlyhoward529@gmail.com>
2022-07-31 12:05:12 -0400

    updated server to connect to sequelize

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const sequelize = require('./config/connection');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT => {
-    console.log(`Listening on PORT # http://localhost:PORT`)
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listening on PORT # http://localhost:PORT`);
+    });
 })
